42815c66c476d79cee17599d35bf6fc3be37bd40
MarinaPolishchuk <me41.harchenko@mail.com>
2023-01-20 09:32:25 +0200

    feat: add js burger menu

--- plants/script.js
+++ plants/script.js
@@ -33,8 +33,9 @@ if (isMobile.any()) {
 }
 //Бургер
 const iconMenu = document.querySelector('.burger-menu');
+const menuBody = document.querySelector('.header__navigation');
+
 if(iconMenu){
-  const menuBody = document.querySelector('.header__navigation');
   iconMenu.addEventListener("click",function(e){
     document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
@@ -43,10 +44,30 @@ if(iconMenu){
 }
 
 //Прокрутка при клике
+const menuLinks = document.querySelectorAll('.navigation__link[data-goto]');
+if(menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click",onMenuLinkClick);
+  });
+
+
 function onMenuLinkClick(e){
   const menuLink=e.target;
   if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
     const gotoBlock=document.querySelector(menuLink.dataset.goto);
-    const gotoBlockValue= gotoBlock.getBoundingClientRect().top + pageYOffset - document
+    const gotoBlockValue= gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+    if(iconMenu.classList.contains('_active')){
+      document.body.classList.remove('_lock');
+      iconMenu.classList.remove('_active');
+      menuBody.classList.remove('_active');
+    }
+
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth"
+    });
+    e.preventDefault();
   }
+ }
 }
