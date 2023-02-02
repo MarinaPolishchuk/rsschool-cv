77b2dde4dfb7b89bd1be0bb7891f2195725ce07c
MarinaPolishchuk <me41.harchenko@mail.com>
2023-02-02 11:40:34 +0200

    feat: add contact section select

--- plants/script.js
+++ plants/script.js
@@ -20,23 +20,23 @@ const isMobile = {
     return (
       isMobile.Android() ||
       isMobile.BlackBerry() ||
-      isMobile.iOS() || 
-      isMobile.Opera() || 
+      isMobile.iOS() ||
+      isMobile.Opera() ||
       isMobile.Windows());
   }
 };
 
 if (isMobile.any()) {
   document.body.classList.add('_touch');
-}else{
+} else {
   document.body.classList.add('_pc');
 }
 //Бургер
 const iconMenu = document.querySelector('.burger-menu');
 const menuBody = document.querySelector('.header__navigation');
 
-if(iconMenu){
-  iconMenu.addEventListener("click",function(e){
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
     document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
     menuBody.classList.toggle('_active');
@@ -45,31 +45,31 @@ if(iconMenu){
 
 //Прокрутка при клике
 const menuLinks = document.querySelectorAll('.navigation__link[data-goto]');
-if(menuLinks.length > 0) {
+if (menuLinks.length > 0) {
   menuLinks.forEach(menuLink => {
-    menuLink.addEventListener("click",onMenuLinkClick);
+    menuLink.addEventListener("click", onMenuLinkClick);
   });
 
 
-function onMenuLinkClick(e){
-  const menuLink=e.target;
-  if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-    const gotoBlock=document.querySelector(menuLink.dataset.goto);
-    const gotoBlockValue= gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
-    if(iconMenu.classList.contains('_active')){
-      document.body.classList.remove('_lock');
-      iconMenu.classList.remove('_active');
-      menuBody.classList.remove('_active');
+      if (iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
     }
-
-    window.scrollTo({
-      top: gotoBlockValue,
-      behavior: "smooth"
-    });
-    e.preventDefault();
   }
- }
 }
 //accordion
 var acc = [...document.getElementsByClassName("price__type")];
@@ -91,3 +91,21 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+// contact form
+const formEl = document.querySelector('.contact__form');
+const formButtonEl = document.querySelector('.contact__button');
+const cities = [...document.getElementsByClassName('city__block')];
+
+let formClick = (e) => {
+  let target = e.target;
+  if (target.classList.contains('city__select')) {
+    formButtonEl.textContent = target.textContent;
+    cities.forEach(c => c.classList.remove('active'));
+    let city = formEl.querySelector('.city__block-' + target.dataset.cityIndex);
+    city.classList.add('active');
+  }
+};
+
+formEl.addEventListener('click', formClick);
